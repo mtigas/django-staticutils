d4d08d3db25d61f28181ce264c9d5cc292405c72
Mike Tigas <mike@tig.as>
2011-11-30 23:58:22 -0800

    implement our overridden collectstatic

---
+++ src/staticutils/finders.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from django.contrib.staticfiles.finders import FileSystemFinder
+from django.core.files.storage import FileSystemStorage
+from django.utils.datastructures import SortedDict
+
+
+class HashedStaticRootFinder(FileSystemFinder):
+    """
+    A hackish version of `FileSystemFinder` that returns resources in
+    `settings.HASHED_STATIC_ROOT`.
+    """
+    def __init__(self, apps=None, *args, **kwargs):
+        # List of locations with static files
+        self.locations = [('', settings.HASHED_STATIC_ROOT), ]
+        # Maps dir paths to an appropriate storage instance
+        self.storages = SortedDict()
+
+        for prefix, root in self.locations:
+            filesystem_storage = FileSystemStorage(location=root)
+            filesystem_storage.prefix = prefix
+            self.storages[root] = filesystem_storage
+        super(HashedStaticRootFinder, self).__init__(*args, **kwargs)
--- src/staticutils/management/commands/collectstatic.py
+++ src/staticutils/management/commands/collectstatic.py
@@ -1,10 +1,16 @@
+import os
+import sys
+from itertools import chain
+from optparse import make_option
+from django.contrib.staticfiles import finders
+from django.utils.encoding import smart_str
 from django.conf import settings
 from django.contrib.staticfiles.management.commands import collectstatic
-from django.core.management.base import CommandError, NoArgsCommand
-from optparse import make_option
+from django.core.management.base import CommandError
+from staticutils.finders import HashedStaticRootFinder
 
-class Command(NoArgsCommand):
-    help = collectstatic.Command.help
+
+class Command(collectstatic.Command):
     option_list = collectstatic.Command.option_list + (
         make_option('--hashed-only',
             action='store_true',
@@ -26,5 +32,71 @@ class Command(NoArgsCommand):
                 "--hashed-only and --plain cannot be used together."
             )
 
-        # Call regular `collectstatic`
-        collectstatic.Command().execute(**options)
+        if hashed_only:
+            # Only use our HashedStaticRootFinder and ignore original behavior.
+            finders_to_use = [HashedStaticRootFinder(), ]
+        elif plain:
+            # The original behavior only.
+            finders_to_use = finders.get_finders()
+        else:
+            # Original behavior + HASHED_STATIC_ROOT. Use chain since
+            # `get_finders` is an iterable (so instantiation is delayed until
+            # the objects are actually needed).
+            finders_to_use = chain(
+                finders.get_finders(),
+                (HashedStaticRootFinder(),)
+            )
+
+        ##### Original handle_noargs #####
+        # Have to copy the whole handle_noargs bit here because it dynamically
+        # uses `django.contrib.staticfiles.finders` at runtime, which we can't
+        # override unless we override the runtime behavior itself.
+        symlink = options['link']
+        ignore_patterns = options['ignore_patterns']
+        if options['use_default_ignore_patterns']:
+            ignore_patterns += ['CVS', '.*', '*~']
+        ignore_patterns = list(set(ignore_patterns))
+        self.verbosity = int(options.get('verbosity', 1))
+
+        if symlink:
+            if sys.platform == 'win32':
+                raise CommandError("Symlinking is not supported by this "
+                                   "platform (%s)." % sys.platform)
+            if not self.local:
+                raise CommandError("Can't symlink to a remote destination.")
+
+        # Warn before doing anything more.
+        if options.get('interactive'):
+            confirm = raw_input(u"""
+You have requested to collect static files at the destination
+location as specified in your settings file.
+
+This will overwrite existing files.
+Are you sure you want to do this?
+
+Type 'yes' to continue, or 'no' to cancel: """)
+            if confirm != 'yes':
+                raise CommandError("Collecting static files cancelled.")
+
+        for finder in finders_to_use:  # MODIFIED
+            for path, storage in finder.list(ignore_patterns):
+                # Prefix the relative path if the source storage contains it
+                if getattr(storage, 'prefix', None):
+                    prefixed_path = os.path.join(storage.prefix, path)
+                else:
+                    prefixed_path = path
+                if symlink:
+                    self.link_file(path, prefixed_path, storage, **options)
+                else:
+                    self.copy_file(path, prefixed_path, storage, **options)
+
+        actual_count = len(self.copied_files) + len(self.symlinked_files)
+        unmodified_count = len(self.unmodified_files)
+        if self.verbosity >= 1:
+            self.stdout.write(smart_str(u"\n%s static file%s %s to '%s'%s.\n"
+                              % (actual_count, actual_count != 1 and 's' or '',
+                                 symlink and 'symlinked' or 'copied',
+                                 settings.STATIC_ROOT,
+                                 unmodified_count and ' (%s unmodified)'
+                                 % unmodified_count or '')))
+        ##### /Original handle_noargs #####
